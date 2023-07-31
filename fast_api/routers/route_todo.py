c72a13e1142e4f8e40fe59fae9454900b0c969b6
Akira <jinnseitanosi@gmail.com>
2023-07-31 09:49:13 +0900

    API done

--- fast_api/routers/route_todo.py
+++ fast_api/routers/route_todo.py
@@ -1,17 +1,26 @@
 from typing import List
-from fastapi import APIRouter, HTTPException, Request, Response
+from fastapi import APIRouter, HTTPException, Request, Response, Depends
 from fastapi.encoders import jsonable_encoder
 from database import db_create_todo, db_delete_todo, db_get_single_todo, db_get_todos, db_update_todo
 from schemas import SuccessMsg, Todo, TodoBody
 from starlette.status import HTTP_201_CREATED
+from fastapi_csrf_protect import CsrfProtect
+from auth_utils import AuthJwtCsrf
 
 router = APIRouter()
+auth = AuthJwtCsrf()
 
 @router.post("/api/todo", response_model=Todo)
-async def create_todo(request: Request, response: Response, data: TodoBody):
+async def create_todo(request: Request, response: Response, data: TodoBody, csrf_protect: CsrfProtect = Depends()):
+    new_token = auth.verify_csrf_update_jwt(
+        request, csrf_protect, request.headers
+    )
     todo = jsonable_encoder(data)
     res = await db_create_todo(todo)
     response.status_code = HTTP_201_CREATED
+    response.set_cookie(
+        key='access_token', value=f"Bearer {new_token}", httponly=True, samesite="none", secure=True
+    )
     
     if res:
         return res
@@ -20,13 +29,20 @@ async def create_todo(request: Request, response: Response, data: TodoBody):
     )
     
 @router.get("/api/todo", response_model=List[Todo])
-async def get_todos():
+async def get_todos(request: Request):
+    auth.verify_jwt(request)
     res = await db_get_todos()
     return res
 
 @router.get("/api/todo/{id}", response_model=Todo)
-async def get_single_todo(id: str):
+async def get_single_todo(request: Request, response: Response, id: str):
+    new_token, _ = auth.verify_update_jwt(request)
     res = await db_get_single_todo(id)
+    
+    response.set_cookie(
+        key='access_token', value=f"Bearer {new_token}", httponly=True, samesite="none", secure=True
+    )
+    
     if res:
         return res
     raise HTTPException(
@@ -34,9 +50,18 @@ async def get_single_todo(id: str):
     )
     
 @router.put("/api/todo/{id}", response_model=Todo)
-async def update_todo(id: str, data: TodoBody):
+async def update_todo(request: Request, response: Response, id: str, data: TodoBody, csrf_protect: CsrfProtect = Depends()):
+    new_token = auth.verify_csrf_update_jwt(
+        request, csrf_protect, request.headers
+    )
+    
     todo = jsonable_encoder(data)
     res = await db_update_todo(id, todo)
+    
+    response.set_cookie(
+        key='access_token', value=f"Bearer {new_token}", httponly=True, samesite="none", secure=True
+    )
+    
     if res:
         return res
     raise HTTPException(
@@ -44,8 +69,17 @@ async def update_todo(id: str, data: TodoBody):
     )
     
 @router.delete("/api/todo/{id}", response_model=SuccessMsg)
-async def delete_todo(id: str):
+async def delete_todo(request: Request, response: Response, id: str, csrf_protect: CsrfProtect = Depends()):
+    new_token = auth.verify_csrf_update_jwt(
+        request, csrf_protect, request.headers
+    )
+
     res = await db_delete_todo(id)
+    
+    response.set_cookie(
+        key='access_token', value=f"Bearer {new_token}", httponly=True, samesite="none", secure=True
+    )
+    
     if res:
         return {'message': 'Successfully deleted'}
     raise HTTPException(
